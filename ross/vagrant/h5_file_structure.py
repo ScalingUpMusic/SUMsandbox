2726636da995fae6aefa3dc5f8d3e6bdb97b4c40
Ross Boberg <ross.boberg@gmail.com>
2015-07-15 21:55:16 -0700

    add file that loops through data strucutre and gives a preview; sample output in README_h5_file_structure

---
+++ ross/vagrant/h5_file_structure.py
@@ -0,0 +1,24 @@
+import h5py
+
+def recursiveKeys(obj, space="", maxcount = 10):
+	try:
+		ks = obj.keys()
+		for k in ks:
+			print space+k
+			recursiveKeys(obj[k], space=space+" ")
+	except AttributeError:
+		count = 0
+		for x in obj:
+			print space+str(x)
+			count += 1
+			if count >= maxcount:
+				print space+'...('+str(len(obj))+')'
+				break
+		return
+
+
+
+file_name = '/vagrant/MillionSongSubset/data/A/X/L/TRAXLZU12903D05F94.h5'
+h5_file = h5py.File(file_name, 'r')
+
+recursiveKeys(h5_file)

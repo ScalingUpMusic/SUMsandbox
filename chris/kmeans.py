43535c4022cd659567561f2a7e200611175c31c8
Chris Walker <cwalker@illuminateed.com>
2015-07-27 16:12:46 -0700

    Fixed feature width issues, truncating to length 10000. Still running out of memory when trying to train model.

--- chris/kmeans.py
+++ chris/kmeans.py
@@ -12,37 +12,36 @@ from pyspark.mllib.clustering import KMeans, KMeansModel
 
 
 k_clusters = 50
-target_shape = (1000*12, 1)
+target_shape = 10000
+max_size = 0
 
-k_means_data = np.empty(target_shape)
+k_means_data = []
 
 
 print "Collecting timbre data..."
 file_list = subprocess.check_output(['/usr/bin/find', '/vagrant/MillionSongSubset/data', '-name', '*.h5', '-print'])
 
 for file_name in file_list.split("\n"):
+    # Skip directories or other non-files
     if not os.path.isfile(file_name):
         continue
     
-    print "\tReading file: %s" % file_name
-    h5_file = h5py.File(file_name, 'r')
-
     # Convert timbre data to 1-dimensional input for K-means
+    h5_file = h5py.File(file_name, 'r')
     file_val = np.array(h5_file['analysis/segments_timbre'])
     
     # Make it 1-dimensional
-    file_val = file_val.reshape((file_val.shape[0] * file_val.shape[1], 1))
+    file_val = file_val.reshape(file_val.shape[0] * file_val.shape[1])[0:target_shape]
     
     # Pad so all songs have same length of features
-    file_val = np.pad(file_val, (0, target_shape[0] - file_val.shape[0]), 'constant', constant_values=(0.0,0.0))
+    file_val = np.pad(file_val, (0, target_shape - file_val.shape[0]), 'constant', constant_values=(0.0,0.0))
     
-    k_means_data = np.append( k_means_data, file_val )
+    # Add to actual feature array
+    k_means_data.append(file_val)
     
     h5_file.close()
 
 
-
-print k_means_data.shape
 
 print("%s total files read...") % len(k_means_data)
 
